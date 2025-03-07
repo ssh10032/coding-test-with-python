8633db5cd81d912e94f151bce0dca0958bf2d577
SongOh <83348645+ssh10032@users.noreply.github.com>
2025-03-07 15:24:43 +0900

    Add files via upload

--- [6] 다이나믹 프로그래밍/연습문제/정수 삼각형/answer.py
+++ [6] 다이나믹 프로그래밍/연습문제/정수 삼각형/answer.py
@@ -4,10 +4,29 @@ sys.stdin = open("input.txt", "r")
 
 n = int(sys.stdin.readline().rstrip())
 
-print(n)
+# print(n)
+d = []
 test = []
 for _ in range(n):
     n_list = list(map(int, sys.stdin.readline().rstrip().split()))
     test.append(n_list)
+    # print(len(n_list))
+    d.append([0] * len(n_list))
 
-print(test)
+d[0][0] = test[0][0]
+d[1][0] = d[0][0] + test[1][0]
+d[1][1] = d[0][0] + test[1][1]
+
+for r in range(2, n):
+    for c in range(len(d[r])):
+        if c == 0:
+            d[r][c] = test[r][c] + d[r-1][c]
+        elif c == len(d[r])-1:
+            d[r][c] = test[r][c] + d[r-1][c-1]
+        else:
+            d[r][c] = test[r][c] + max(d[r-1][c-1], d[r-1][c])
+
+# print(test)
+# print(d)
+
+print(max(d[n-1]))
