a2e65714f5d5ff3157b858c412f0ac1ec9c47806
SongOh <83348645+ssh10032@users.noreply.github.com>
2025-04-18 16:05:57 +0900

    Add files via upload

--- [2] 구현/복습/실전문제/게임 개발/answer3.py
+++ [2] 구현/복습/실전문제/게임 개발/answer3.py
@@ -2,3 +2,70 @@ import sys
 
 sys.stdin = open("input.txt", "r")
 
+n, m = map(int, sys.stdin.readline().rstrip().split())
+
+# 방문한 위치를 저장하기 위한 맵을 생성하여 0으로 초기화
+d = [[0] * m for _ in range(n)]
+
+# 현재 캐릭터의 좌표, 방향 입력
+x, y, direction = map(int, sys.stdin.readline().rstrip().split())
+
+d[x][y] = 1
+
+array = []
+for i in range(n):
+    array.append(list(map(int, sys.stdin.readline().rstrip().split())))
+
+# 북 동 남 서 방향 정의
+dx = [-1, 0, 1, 0]
+dy = [0, 1, 0, -1]
+
+# 왼쪽으로 회전
+def turn_left():
+    global direction
+    direction -=1
+    if direction == -1:
+        direction=3
+
+count = 1
+turn_time = 0
+while True:
+    turn_left()
+    nx = x + dx[direction]
+    ny = y + dy[direction]
+    if d[nx][ny] == 0 and array[nx][ny]==0:
+        d[nx][ny] = 1
+        x = nx
+        y = ny
+        count+=1
+        turn_time=0
+        continue
+    else:
+        turn_time+=1
+
+    if turn_time == 4:
+        nx = x - dx[direction]
+        ny = y - dy[direction]
+
+        if array[nx][ny]==0:
+            x = nx
+            y = ny
+        else:
+            break
+        turn_time = 0
+
+print(count)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
