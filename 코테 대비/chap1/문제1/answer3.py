e06b70b6546260291f16a311415de78e59c0dc16
SongOh <83348645+ssh10032@users.noreply.github.com>
2025-08-18 14:29:38 +0900

    Add files via upload

---
+++ 코테 대비/chap1/문제1/answer3.py
@@ -0,0 +1,19 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+n = int(sys.stdin.readline().rstrip())
+
+def find_prime_list_under_number(number):
+    prime_list = []
+
+    for n in range(2, number+1):
+        for i in prime_list:
+            if i * i <= n and n % i == 0:
+                break
+        else:
+            prime_list.append(n)
+
+    return prime_list
+
+print(find_prime_list_under_number(n))
---
+++ 코테 대비/chap1/문제2/answer1.py
@@ -0,0 +1,24 @@
+import sys
+
+sys.stdin = open("input.txt", "r")
+
+input = str(sys.stdin.readline().rstrip())
+
+def find_count_to_turn_out_to_all_zero_or_all_one(string):
+    count_to_all_zero = 0
+    count_to_all_one = 0
+
+    if string[0]=='0':
+        count_to_all_one += 1
+    elif string[0]=='1':
+        count_to_all_zero +=1
+
+    for i in range(len(string)-1):
+        if string[i]!=string[i+1]:
+            if string[i+1]=='0':
+                count_to_all_one+=1
+            if string[i+1]=='1':
+                count_to_all_zero+=1
+    return min(count_to_all_zero, count_to_all_one)
+
+print(find_count_to_turn_out_to_all_zero_or_all_one(input))
